8ef281360320b97d8a4e19f9898cf6605812b4e5
WATANABE Yuki <magicant@wonderwand.net>
2014-02-15 18:36:09 +0900

    Add SpecificOperatorParser class

---
+++ src/language/parser/SpecificOperatorParser.hh
@@ -0,0 +1,62 @@
+/* Copyright (C) 2014 WATANABE Yuki
+ *
+ * This file is part of Sesh.
+ *
+ * Sesh is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Sesh is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Sesh.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#ifndef INCLUDED_language_parser_SpecificOperatorParser_hh
+#define INCLUDED_language_parser_SpecificOperatorParser_hh
+
+#include "buildconfig.h"
+
+#include "common/String.hh"
+#include "language/parser/LineContinuationTreatment.hh"
+#include "language/parser/Operator.hh"
+#include "language/parser/OperatorParser.hh"
+#include "language/parser/Parser.hh"
+
+namespace sesh {
+namespace language {
+namespace parser {
+
+/** Parses a specific operator that is specified in the constructor call. */
+class SpecificOperatorParser : public Parser<Operator> {
+
+private:
+
+    Operator mAcceptedOperator;
+    OperatorParser mAnyOperatorParser;
+
+public:
+
+    explicit SpecificOperatorParser(
+            Environment &,
+            Operator,
+            LineContinuationTreatment = LineContinuationTreatment::REMOVE);
+
+    void parseImpl() final override;
+
+    common::Maybe<Operator> &result() noexcept final override;
+
+    void resetImpl() noexcept final override;
+
+}; // class SpecificOperatorParser
+
+} // namespace parser
+} // namespace language
+} // namespace sesh
+
+#endif // #ifndef INCLUDED_language_parser_SpecificOperatorParser_hh
+
+/* vim: set et sw=4 sts=4 tw=79 cino=\:0,g0,N-s,i2s,+2s: */
---
+++ src/language/parser/SpecificOperatorParserTest.cc
@@ -0,0 +1,83 @@
+/* Copyright (C) 2014 WATANABE Yuki
+ *
+ * This file is part of Sesh.
+ *
+ * Sesh is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Sesh is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Sesh.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include "buildconfig.h"
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include <utility>
+#include "common/String.hh"
+#include "language/parser/EofEnvironment.hh"
+#include "language/parser/EnvironmentTestHelper.hh"
+#include "language/parser/LineContinuationEnvironment.hh"
+#include "language/parser/Operator.hh"
+#include "language/parser/SpecificOperatorParser.hh"
+
+namespace sesh {
+namespace language {
+namespace parser {
+
+using sesh::common::String;
+using sesh::language::parser::EofEnvironment;
+using sesh::language::parser::LineContinuationEnvironment;
+using sesh::language::parser::Operator;
+using sesh::language::parser::SourceTestEnvironment;
+using sesh::language::parser::SpecificOperatorParser;
+
+class SpecificOperatorParserTestEnvironment :
+        public SourceTestEnvironment,
+        public EofEnvironment,
+        public LineContinuationEnvironment {
+};
+
+TEST_CASE("Specific operator parser, construction") {
+    SpecificOperatorParserTestEnvironment e;
+    SpecificOperatorParser p(e, Operator::operatorAnd());
+    SpecificOperatorParser(std::move(p));
+}
+
+TEST_CASE("Specific operator parser, accepted operator") {
+    SpecificOperatorParserTestEnvironment e;
+    SpecificOperatorParser p(e, Operator::operatorAnd());
+    e.appendSource(String(Operator::AND));
+    e.setIsEof();
+    REQUIRE(p.parse().hasValue());
+    CHECK(p.parse().value() == Operator::operatorAnd());
+    CHECK(e.position() == e.length());
+}
+
+TEST_CASE("Specific operator parser, unaccepted operator") {
+    SpecificOperatorParserTestEnvironment e;
+    SpecificOperatorParser p(e, Operator::operatorAnd());
+    e.appendSource(String(Operator::AND_AND));
+    e.setIsEof();
+    CHECK_FALSE(p.parse().hasValue());
+}
+
+TEST_CASE("Specific operator parser, not a operator") {
+    SpecificOperatorParserTestEnvironment e;
+    SpecificOperatorParser p(e, Operator::operatorAnd());
+    e.appendSource(L("-"));
+    CHECK_FALSE(p.parse().hasValue());
+}
+
+} // namespace parser
+} // namespace language
+} // namespace sesh
+
+/* vim: set et sw=4 sts=4 tw=79 cino=\:0,g0,N-s,i2s,+2s: */
