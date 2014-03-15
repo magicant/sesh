db67298e06e6f4452071cb5ca75b202b8880d847
WATANABE Yuki <magicant@wonderwand.net>
2014-03-15 14:54:40 +0900

    Use --enable-debug-build option for Jenkins build

--- .jenkins-valgrind.sh
+++ .jenkins-valgrind.sh
@@ -1,13 +1,11 @@
 set -eu
 
-export CXXFLAGS='-g'
-
 for tarball in *-*.tar.gz
 do
   gzip -dc "$tarball" | tar -x -f -
 done
 cd *-*/
 
-sh configure --disable-dependency-tracking
+sh configure --disable-dependency-tracking --enable-debug-build
 trap 'cat src/test-suite.log' EXIT
 make -k LOG_COMPILER='$(SHELL) $(top_srcdir)/../.jenkins-log.sh' check
