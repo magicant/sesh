a1207f1071b99be5df4018cd5c2200309028ef60
WATANABE Yuki <magicant@wonderwand.net>
2014-03-21 15:16:24 +0900

    Update Jenkins build script for new Makefile
    The test-suite.log file is now in the top directory due to the Makefile
    structure change in the previous commit.

--- .jenkins-valgrind.sh
+++ .jenkins-valgrind.sh
@@ -7,5 +7,5 @@ done
 cd *-*/
 
 sh configure --disable-dependency-tracking --enable-debug-build
-trap 'cat src/test-suite.log' EXIT
+trap 'cat test-suite.log' EXIT
 make -k LOG_COMPILER='$(SHELL) $(top_srcdir)/../.jenkins-log.sh' check
