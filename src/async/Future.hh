d593f66e4744bba1d34b2a3bf091a2f6860abdd8
WATANABE Yuki <magicant@wonderwand.net>
2014-03-30 01:19:32 +0900

    Add Future class template

---
+++ src/async/Future.hh
@@ -0,0 +1,221 @@
+/* Copyright (C) 2014 WATANABE Yuki
+ *
+ * This file is part of Sesh.
+ *
+ * Sesh is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Sesh is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Sesh.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#ifndef INCLUDED_async_Future_hh
+#define INCLUDED_async_Future_hh
+
+#include "buildconfig.h"
+
+#include <exception>
+#include <functional>
+#include <memory>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include "async/Delay.hh"
+#include "async/DelayHolder.hh"
+#include "async/Promise.hh"
+#include "async/Result.hh"
+
+namespace sesh {
+namespace async {
+
+namespace future_impl {
+
+template<typename T>
+class Future;
+
+/** A base class that implements most part of the future class. */
+template<typename T>
+class FutureBase : public DelayHolder<T> {
+
+public:
+
+    using Callback = typename Delay<T>::Callback;
+
+    using DelayHolder<T>::DelayHolder;
+
+    /**
+     * Sets a callback function to receive the result from the associated
+     * promise. After the callback is set, this future instance will have no
+     * associated promise.
+     *
+     * The behavior is undefined if this future instance has no associated
+     * promise.
+     */
+    void setCallback(Callback &&) &&;
+
+    /**
+     * Sets a callback function that converts the result to another result.
+     *
+     * The argument callback will be called when this future receives a result.
+     * The result of the callback will become the result of the future returned
+     * from this method.
+     *
+     * If this future receives an exception, the callback will not be called
+     * and the exception will be propagated to the future returned from this
+     * method.
+     *
+     * If the callback throws an exception, that will be propagated alike.
+     *
+     * @tparam F parameter type of the callback function
+     * @tparam G object type of the callback
+     * @tparam R result type of the callback
+     */
+    template<
+            typename F,
+            typename G = typename std::decay<F>::type,
+            typename R = typename std::result_of<G(T &&)>::type>
+    Future<R> then(F &&) &&;
+
+    /**
+     * Sets a callback function that recovers this future from an exception.
+     *
+     * If this future receives a result of type T, then the argument callback
+     * will not be called and the future returned from this method will receive
+     * the same result.
+     *
+     * If this future returned an exception, the callback is called with a
+     * pointer to the exception. The result of the callback will become the
+     * result of the future returned from this method.
+     *
+     * If the callback function or the move-constructor of the result throws an
+     * exception, that will be propagated to the returned future.
+     *
+     * @tparam F Parameter type of the callback function. It must be callable
+     * with an exception pointer parameter and return a return of type T.
+     * @tparam G Object type of the callback.
+     */
+    template<
+            typename F,
+            typename G = typename std::decay<F>::type>
+    Future<T> recover(F &&) &&;
+
+    /**
+     * Adds a callback to this future so that its result will be set to the
+     * argument promise.
+     */
+    void forward(Promise<T> &&) &&;
+
+}; // template<typename T> class FutureBase
+
+/**
+ * The output side of a future/promise pair.
+ *
+ * You can set a callback function to the future to asynchronously receive the
+ * result of computation from the associated promise.
+ *
+ * A future instance has no associated promise if it was default-constructed or
+ * a callback has been set.
+ *
+ * Futures are not copyable to prevent setting multiple callbacks. The shared
+ * future class, however, allows setting multiple callbacks for a single
+ * promise.
+ *
+ * @tparam T The result type. It must be a decayed type other than
+ * std::exception_ptr.
+ */
+template<typename T>
+class Future : public FutureBase<T> {
+
+public:
+
+    using FutureBase<T>::FutureBase;
+
+}; // template<typename T> class Future
+
+/** A specialization of the future class that has the unwrap method. */
+template<typename T>
+class Future<Future<T>> : public FutureBase<Future<T>> {
+
+public:
+
+    using FutureBase<Future<T>>::FutureBase;
+
+    /**
+     * Unwraps this nested future. The returned future will receive the same
+     * result as the inner future.
+     */
+    Future<T> unwrap() &&;
+
+}; // template<typename T> class Future<Future<T>>
+
+/**
+ * Creates a pair of new promise and future that are associated with each
+ * other.
+ */
+template<typename T>
+std::pair<Promise<T>, Future<T>> createPromiseFuturePair();
+
+/**
+ * Constructs a future that has already received a result from the argument
+ * function. The argument function is called to construct the result of the
+ * returned future. If the argument function throws an exception, it is set to
+ * the returned future.
+ *
+ * @tparam F type of the argument function that returns a value (or throws an
+ * exception) that becomes the result of the returned future.
+ */
+template<typename F>
+auto createFutureFrom(F &&) -> Future<typename std::result_of<F()>::type>;
+
+/**
+ * Constructs a future that has already received a result constructed from
+ * the arguments. If the constructor of the result throws an exception, it
+ * is set to the returned future.
+ *
+ * @tparam T type of the result of the returned future.
+ * @tparam Arg argument types.
+ * @param arg arguments that are passed to the constructor of T.
+ */
+template<typename T, typename... Arg>
+Future<T> createFuture(Arg &&... arg);
+
+/**
+ * Constructs a future whose result has been set to a copy of the argument. If
+ * the copy- or move-constructor of the result throws an exception, it is set
+ * to the returned future.
+ */
+template<typename T>
+auto createFutureOf(T &&) -> Future<typename std::decay<T>::type>;
+
+/** Constructs a future whose result has been set to the argument exception. */
+template<typename T>
+Future<T> createFailedFuture(std::exception_ptr);
+
+/** Constructs a future whose result has been set to the argument exception. */
+template<typename T, typename E>
+Future<T> createFailedFutureOf(E &&e);
+
+} // namespace future_impl
+
+using future_impl::Future;
+using future_impl::createFailedFuture;
+using future_impl::createFailedFutureOf;
+using future_impl::createFuture;
+using future_impl::createFutureFrom;
+using future_impl::createFutureOf;
+using future_impl::createPromiseFuturePair;
+
+} // namespace async
+} // namespace sesh
+
+#include "Future.tcc"
+
+#endif // #ifndef INCLUDED_async_Future_hh
+
+/* vim: set et sw=4 sts=4 tw=79 cino=\:0,g0,N-s,i2s,+2s: */
---
+++ src/async/FutureTest.cc
@@ -0,0 +1,374 @@
+/* Copyright (C) 2014 WATANABE Yuki
+ *
+ * This file is part of Sesh.
+ *
+ * Sesh is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Sesh is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Sesh.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include "buildconfig.h"
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include <exception>
+#include <tuple>
+#include <utility>
+#include "async/Delay.hh"
+#include "async/Future.hh"
+#include "async/Promise.hh"
+#include "async/Result.hh"
+
+namespace {
+
+using sesh::async::Delay;
+using sesh::async::Future;
+using sesh::async::Promise;
+using sesh::async::Result;
+using sesh::async::createFailedFutureOf;
+using sesh::async::createFuture;
+using sesh::async::createFutureFrom;
+using sesh::async::createFutureOf;
+using sesh::async::createPromiseFuturePair;
+
+struct MoveOnly {
+    MoveOnly() = default;
+    MoveOnly(MoveOnly &&) = default;
+};
+
+TEST_CASE("Future, default construction and invalidness") {
+    Future<Future<int>> f;
+    CHECK_FALSE(f.isValid());
+}
+
+TEST_CASE("Future, construction and validness") {
+    auto delay = std::make_shared<Delay<int>>();
+    Future<int> f(delay);
+    delay = nullptr;
+    CHECK(f.isValid());
+}
+
+TEST_CASE("Future, invalidness after setting callback") {
+    const auto delay = std::make_shared<Delay<int>>();
+    Future<int> f(delay);
+    std::move(f).setCallback([](Result<int> &&) {});
+    CHECK_FALSE(f.isValid());
+}
+
+TEST_CASE("Future, setting callback") {
+    const auto delay = std::make_shared<Delay<int>>();
+    Future<int> f(delay);
+
+    int i = 0;
+    std::move(f).setCallback([&i](Result<int> &&r) { i = *r; });
+
+    CHECK(i == 0);
+    delay->setResultFrom([] { return 1; });
+    CHECK(i == 1);
+}
+
+TEST_CASE("Create promise/future pair") {
+    std::pair<Promise<int>, Future<int>> &&pf = createPromiseFuturePair<int>();
+    std::move(pf.first).setResultFrom([] { return 123; });
+
+    int i = 0;
+    std::move(pf.second).setCallback([&i](Result<int> &&r) { i = *r; });
+    CHECK(i == 123);
+}
+
+TEST_CASE("Future, then, success, movable function") {
+    class MovableFunction {
+    private:
+        int &mI;
+    public:
+        explicit MovableFunction(int &i) noexcept : mI(i) { }
+        MovableFunction(MovableFunction &&) = default;
+        double operator()(int &&v) noexcept {
+            mI = v;
+            return 2.0;
+        }
+    };
+
+    const auto delay = std::make_shared<Delay<int>>();
+    Future<int> f1(delay);
+
+    int i = 0;
+    Future<double> f2 = std::move(f1).then(MovableFunction(i));
+
+    double d = 0.0;
+    std::move(f2).setCallback([&d](Result<double> &&r) { d = *r; });
+
+    CHECK(i == 0);
+    CHECK(d == 0.0);
+    delay->setResultFrom([] { return 1; });
+    CHECK(i == 1);
+    CHECK(d == 2.0);
+}
+
+TEST_CASE("Future, then, success, copyable constant function") {
+    const auto delay = std::make_shared<Delay<int>>();
+    Future<int> f1(delay);
+
+    int i = 0;
+    const auto f = [&i](int &&v) -> double { i = v; return 2.0; };
+    Future<double> f2 = std::move(f1).then(f);
+
+    double d = 0.0;
+    std::move(f2).setCallback([&d](Result<double> &&r) { d = *r; });
+
+    CHECK(i == 0);
+    CHECK(d == 0.0);
+    delay->setResultFrom([] { return 1; });
+    CHECK(i == 1);
+    CHECK(d == 2.0);
+}
+
+TEST_CASE("Future, then, failure propagation") {
+    const auto delay = std::make_shared<Delay<int>>();
+    Future<int> f1(delay);
+
+    const auto f = [](int &&) -> char { FAIL("unexpected"); return 'a'; };
+    Future<char> f2 = std::move(f1).then(f);
+
+    double d = 0.0;
+    std::move(f2).setCallback([&d](Result<char> &&r) {
+        try {
+            *r;
+        } catch (double v) {
+            d = v;
+        }
+    });
+
+    CHECK(d == 0.0);
+    delay->setResultFrom([]() -> int { throw 2.0; });
+    CHECK(d == 2.0);
+}
+
+TEST_CASE("Future, then, failure in callback") {
+    const auto delay = std::make_shared<Delay<int>>();
+    Future<int> f1(delay);
+
+    int i = 0;
+    const auto f = [&i](int &&v) -> char { i = v; throw 2.0; };
+    Future<char> f2 = std::move(f1).then(f);
+
+    double d = 0.0;
+    std::move(f2).setCallback([&d](Result<char> &&r) {
+        try {
+            *r;
+        } catch (double v) {
+            d = v;
+        }
+    });
+
+    CHECK(i == 0);
+    CHECK(d == 0.0);
+    delay->setResultFrom([] { return 1; });
+    CHECK(i == 1);
+    CHECK(d == 2.0);
+}
+
+TEST_CASE("Future, recover, success, movable function") {
+    class MovableFunction {
+    public:
+        MovableFunction() = default;
+        MovableFunction(MovableFunction &&) = default;
+        int operator()(std::exception_ptr) {
+            FAIL("unexpected exception");
+            return 2;
+        }
+    };
+
+    const auto delay = std::make_shared<Delay<int>>();
+    Future<int> f1(delay);
+    Future<int> f2 = std::move(f1).recover(MovableFunction());
+
+    int i = 0;
+    std::move(f2).setCallback([&i](Result<int> &&r) { i = *r; });
+
+    CHECK(i == 0);
+    delay->setResultFrom([] { return 1; });
+    CHECK(i == 1);
+}
+
+TEST_CASE("Future, recover, success, copyable constant function") {
+    const auto delay = std::make_shared<Delay<int>>();
+    Future<int> f1(delay);
+    const auto f = [](std::exception_ptr) -> int {
+        FAIL("unexpected exception");
+        return 2;
+    };
+    Future<int> f2 = std::move(f1).recover(f);
+
+    int i = 0;
+    std::move(f2).setCallback([&i](Result<int> &&r) { i = *r; });
+
+    CHECK(i == 0);
+    delay->setResultFrom([] { return 1; });
+    CHECK(i == 1);
+}
+
+TEST_CASE("Future, recover from exception") {
+    const auto delay = std::make_shared<Delay<int>>();
+    Future<int> f1(delay);
+    const auto f = [](std::exception_ptr e) -> int {
+        try {
+            std::rethrow_exception(e);
+        } catch (double d) {
+            CHECK(d == 1.0);
+            return 1;
+        }
+        throw 2.0;
+    };
+    Future<int> f2 = std::move(f1).recover(f);
+
+    int i = 0;
+    std::move(f2).setCallback([&i](Result<int> &&r) { i = *r; });
+
+    CHECK(i == 0);
+    delay->setResultFrom([]() -> int { throw 1.0; });
+    CHECK(i == 1);
+}
+
+TEST_CASE("Future, recovery failure") {
+    const auto delay = std::make_shared<Delay<int>>();
+    Future<int> f1(delay);
+    const auto f = [](std::exception_ptr) -> int { throw 2.0; };
+    Future<int> f2 = std::move(f1).recover(f);
+
+    int i = 0;
+    std::move(f2).setCallback([&i](Result<int> &&r) {
+        try {
+            *r;
+        } catch (double d) {
+            CHECK(d == 2.0);
+            i = 1;
+        }
+    });
+
+    CHECK(i == 0);
+    delay->setResultFrom([]() -> int { throw 1.0; });
+    CHECK(i == 1);
+}
+
+TEST_CASE("Future, create from function") {
+    int i = 0;
+    Future<int> f = createFutureFrom([] { return 42; });
+    std::move(f).setCallback([&i](Result<int> &&r) { i = *r; });
+    CHECK(i == 42);
+}
+
+TEST_CASE("Future, create by result construction, l-value") {
+    using T = std::tuple<int, char, double>;
+    Future<T> f = createFuture<T>(1, 'a', 2.0);
+    int i = 0;
+    char c = '0';
+    double d = 0.0;
+    std::move(f).setCallback([&i, &c, &d](Result<T> &&r) {
+        std::tie(i, c, d) = *r;
+    });
+    CHECK(i == 1);
+    CHECK(c == 'a');
+    CHECK(d == 2.0);
+}
+
+TEST_CASE("Future, create by result construction, r-value") {
+    createFuture<MoveOnly>(MoveOnly());
+}
+
+TEST_CASE("Future, create from existing value") {
+    bool called = false;
+    createFutureOf(MoveOnly()).setCallback([&called](Result<MoveOnly> &&r) {
+        CHECK_NOTHROW(*r);
+        called = true;
+    });
+    CHECK(called);
+}
+
+TEST_CASE("Future, create from exception") {
+    bool called = false;
+    createFailedFutureOf<int>(1.0).setCallback([&called](Result<int> &&r) {
+        try {
+            *r;
+        } catch (double d) {
+            CHECK(d == 1.0);
+            called = true;
+        }
+    });
+    CHECK(called);
+}
+
+TEST_CASE("Future, forward, success") {
+    std::pair<Promise<int>, Future<int>> pf1 = createPromiseFuturePair<int>();
+    std::pair<Promise<int>, Future<int>> pf2 = createPromiseFuturePair<int>();
+    std::move(pf1.first).setResultFrom([] { return 123; });
+    std::move(pf1.second).forward(std::move(pf2.first));
+
+    int i = 0;
+    std::move(pf2.second).setCallback([&i](Result<int> &&r) { i = *r; });
+    CHECK(i == 123);
+}
+
+TEST_CASE("Future, forward, failure") {
+    std::pair<Promise<int>, Future<int>> pf1 = createPromiseFuturePair<int>();
+    std::pair<Promise<int>, Future<int>> pf2 = createPromiseFuturePair<int>();
+    std::move(pf1.first).setResultFrom([]() -> int { throw 1.0; });
+    std::move(pf1.second).forward(std::move(pf2.first));
+
+    double d = 0.0;
+    std::move(pf2.second).setCallback([&d](Result<int> &&r) {
+        try {
+            *r;
+        } catch (double v) {
+            d = v;
+        }
+    });
+    CHECK(d == 1.0);
+}
+
+TEST_CASE("Future, unwrap, success") {
+    Future<int> f = createFutureOf(createFutureOf(123)).unwrap();
+    int i = 0;
+    std::move(f).setCallback([&i](Result<int> &&r) { i = *r; });
+    CHECK(i == 123);
+}
+
+TEST_CASE("Future, unwrap, failure in first") {
+    Future<int> f = createFutureOf(createFailedFutureOf<int>(1.0)).unwrap();
+    double d = 0.0;
+    std::move(f).setCallback([&d](Result<int> &&r) {
+        try {
+            *r;
+        } catch (double v) {
+            d = v;
+        }
+    });
+    CHECK(d == 1.0);
+}
+
+TEST_CASE("Future, unwrap, failure in second") {
+    double d = 0.0;
+    createFutureOf(0).then([](int &&) -> Future<int> {
+        throw 1.0;
+    }).unwrap().setCallback([&d](Result<int> &&r) {
+        try {
+            *r;
+        } catch (double v) {
+            d = v;
+        }
+    });
+    CHECK(d == 1.0);
+}
+
+} // namespace
+
+/* vim: set et sw=4 sts=4 tw=79 cino=\:0,g0,N-s,i2s,+2s: */
