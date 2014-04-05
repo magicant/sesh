5e46fda8f83eaf011557b70405a4b01f6f8b313c
WATANABE Yuki <magicant@wonderwand.net>
2014-04-05 22:07:10 +0900

    Add RealApi class

---
+++ src/os/RealApi.hh
@@ -0,0 +1,39 @@
+/* Copyright (C) 2014 WATANABE Yuki
+ *
+ * This file is part of Sesh.
+ *
+ * Sesh is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Sesh is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Sesh.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#ifndef INCLUDED_os_RealApi_hh
+#define INCLUDED_os_RealApi_hh
+
+#include "buildconfig.h"
+
+#include "os/Api.hh"
+
+namespace sesh {
+namespace os {
+
+class RealApi : public Api {
+
+    std::error_condition close(io::FileDescriptor &) const override;
+
+}; // class RealApi
+
+} // namespace os
+} // namespace sesh
+
+#endif // #ifndef INCLUDED_os_RealApi_hh
+
+/* vim: set et sw=4 sts=4 tw=79 cino=\:0,g0,N-s,i2s,+2s: */
